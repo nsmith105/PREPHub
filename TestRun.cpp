33f295c7c3181c9a7e78ff3b923ee076a5e5c26c
nicksmith105 <30782766+nicksmith105@users.noreply.github.com>
2019-05-06 18:34:43 -0700

    base file added

---
+++ TestRun.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool returnABool(bool aThing){}
+int returnANum(int aNum){}
+int returnAnotherNum(int anotherNum) {}
+char returnAChar(char aChar) {}
+char thisIsAlsoAChar(char aChar) {}
+float thisIsAFloat(float aFloat) {}
+
+int main() {
+    int num1, num2;
+    num1 = num2 = 0;
+    float num3 = 0;
+    char letter1 = a;
+    char letter2 = b;
+    bool aBool = false;
+
+    returnABool(aBool)
+    print(aBool);
+
+    returnANum(num1);
+    print(num1);
+
+    returnAnotherNum(num2);
+    print(num2);
+
+    thisIsAFloat(num3);
+    print(num3);
+
+    returnAChar(letter1);
+    print(letter1);
+
+    thisIsAlsoAChar(letter2);
+    print(letter2);
+
+    return 0;
+}
