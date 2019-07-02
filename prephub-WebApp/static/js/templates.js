805b4f3a74a5777f273563fc5650fc890821fcff
Haison Nguyen <hat@Haisons-MacBook-Pro.local>
2019-07-01 20:26:52 -0700

    converted dom manipulation to jquery

--- prephub-WebApp/static/js/templates.js
+++ prephub-WebApp/static/js/templates.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+  /**
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -12,7 +14,21 @@ for (i = 0; i < coll.length; i++) {
         }
     });
 }
-
+ */
+  let $collapsible = $('.collapsible');
+  
+  $collapsible.on('click',function() {
+    let $this = $(this);
+    let content = $this.next();
+    if (content.css("max-height") != "0px") {
+      $this.removeClass("active");
+      content.css("max-height", 0);
+    } else {
+      $this.addClass("active");
+      content.css("max-height", content.prop("scrollHeight"));
+    }
+  });
+});
 
 /**
  * this is the same as saying $selector = jQuery('selector');
@@ -25,34 +41,40 @@ for (i = 0; i < coll.length; i++) {
 
  */
 
+/*
 function rClick(button) {
-    buttons = document.getElementsByClassName("btn-group")[0].getElementsByClassName("button");
-    var i;
-    for (i = 0; i < buttons.length; i++) {
-        if (buttons[i] == button) {
-            buttons[i].disabled = true;
-            buttons[i].style.background = "#013220";
-            buttons[i].style.color = "white"
-        } else if (buttons[i].disabled == true) {
-            buttons[i].disabled = false;
-            buttons[i].style.background = "#669900";
-            buttons[i].style.color = "black"
-        }
+  buttons = document
+    .getElementsByClassName("btn-group")[0]
+    .getElementsByClassName("button");
+  var i;
+  for (i = 0; i < buttons.length; i++) {
+    if (buttons[i] == button) {
+      buttons[i].disabled = true;
+      buttons[i].style.background = "#013220";
+      buttons[i].style.color = "white";
+    } else if (buttons[i].disabled == true) {
+      buttons[i].disabled = false;
+      buttons[i].style.background = "#669900";
+      buttons[i].style.color = "black";
     }
+  }
 }
 
 function lClick(button) {
-    buttons = document.getElementsByClassName("btn-group")[1].getElementsByClassName("button");
-    var i;
-    for (i = 0; i < buttons.length; i++) {
-        if (buttons[i] == button) {
-            buttons[i].disabled = true;
-            buttons[i].style.background = "#013220";
-            buttons[i].style.color = "white"
-        } else if (buttons[i].disabled == true) {
-            buttons[i].disabled = false;
-            buttons[i].style.background = "#669900";
-            buttons[i].style.color = "black"
-        }
+  buttons = document
+    .getElementsByClassName("btn-group")[1]
+    .getElementsByClassName("button");
+  var i;
+  for (i = 0; i < buttons.length; i++) {
+    if (buttons[i] == button) {
+      buttons[i].disabled = true;
+      buttons[i].style.background = "#013220";
+      buttons[i].style.color = "white";
+    } else if (buttons[i].disabled == true) {
+      buttons[i].disabled = false;
+      buttons[i].style.background = "#669900";
+      buttons[i].style.color = "black";
     }
+  }
 }
+*/
