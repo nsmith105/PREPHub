f4eabd168ca9ef09979f953c896d6a2f65ccd229
rhorrace <rhorrace@pdx.edu>
2019-07-08 14:29:59 -0700

    Add files via upload

--- prephub-WebApp/app.py
+++ prephub-WebApp/app.py
@@ -1,19 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, request, url_for, redirect, render_template
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+global local_flag
+local_flag = None
+
+@app.route("/web")
+def web():
+  global local_flag
+  local_flag = False
+  return render_template("web.html")
+
+@app.route("/local")
+def local():
+  global local_flag
+  local_flag = True
+  return render_template("local.html")
+
+@app.route("/logout")
+def logout_press():
+  if local_flag: return redirect(url_for("local"))
+  else: return redirect(url_for("web"))
 
 @app.route("/login")
 def login_press():
   return render_template("login.html")
 
-@app.route("/")
-def logout_press():
-  return render_template("local.html")
-
-@app.route("/")
+@app.route("/cancel")
 def cancel_press():
-  return render_template("local.html")
+  if local_flag: return redirect(url_for("local"))
+  else: return redirect(url_for("web"))
 
 @app.route("/admin")
 def submit_press():
@@ -21,7 +38,7 @@ def submit_press():
 
 @app.route("/")
 def render_static():
-  return render_template("local.html")
+  return render_template("access.html")
 
 if __name__ == '__main__':
     Bootstrap(app)
--- prephub-WebApp/static/js/local.js
+++ prephub-WebApp/static/js/local.js
@@ -35,27 +35,29 @@ $(document).ready(function () {
   });
 
   $('.twitter').slick({
-    dots: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-    ]
+  dots: true,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+  ]
   });
 });
+
+
