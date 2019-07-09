d5d3a8a768d5ee3d08e63b89d50c035b950b9078
rhorrace <rhorrace@pdx.edu>
2019-07-08 20:03:26 -0700

    Add files via upload

--- prephub-WebApp/app.py
+++ prephub-WebApp/app.py
@@ -3,25 +3,14 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
-global local_flag
-local_flag = None
 
-@app.route("/web")
-def web():
-  global local_flag
-  local_flag = False
-  return render_template("web.html")
-
-@app.route("/local")
+@app.route("/")
 def local():
-  global local_flag
-  local_flag = True
   return render_template("local.html")
 
 @app.route("/logout")
 def logout_press():
-  if local_flag: return redirect(url_for("local"))
-  else: return redirect(url_for("web"))
+  return redirect(url_for("local"))
 
 @app.route("/login")
 def login_press():
@@ -29,8 +18,7 @@ def login_press():
 
 @app.route("/cancel")
 def cancel_press():
-  if local_flag: return redirect(url_for("local"))
-  else: return redirect(url_for("web"))
+  return redirect(url_for("local"))
 
 @app.route("/admin")
 def submit_press():
@@ -38,7 +26,7 @@ def submit_press():
 
 @app.route("/")
 def render_static():
-  return render_template("access.html")
+  return render_template("local.html")
 
 if __name__ == '__main__':
     Bootstrap(app)
