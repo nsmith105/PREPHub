688dd8e013d7649b45e17089b99e2b6b3041e8b2
Haison Nguyen <hat@Haisons-MacBook-Pro.local>
2019-08-05 16:21:47 -0700

    merged webapp to server folder

--- prephub-WebApp/app.js
+++ prephub-WebApp/app.js
@@ -1,62 +1,43 @@
-
+'use strict'
 var express = require('express');
-var app = express(); // Need this line to call express functions
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var bodyParser = require('body-parser'); // Need this to parse body of a request
-
-
-// Namespaces
-var ioWeb = io.of('/user');
-var ioPi = io.of('/pi');
-
+var app = express();
+var bodyParser = require('body-parser');
+var http = require('http').Server(app);
+var socket = require('socket.io')(http);
+// Socket IO Namespaces
+var ioWeb = socket.of('/web');
+var ioPi = socket.of('/pi');
 /********* Express Routing *********/
 app.use(express.static(__dirname));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.use(bodyParser.json());
-
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/templates/local.html');
 });
 
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/templates/login.html'); 
+app.post('/twitter', (req, res) => {
+  console.log("to send")
+  ioWeb.emit('twitter data', req.body);
 });
 
-app.post('/admin', (req, res) => {
-    console.log(req.body);
-    res.sendFile(__dirname + '/templates/admin.html');
-});
-
-app.get('/logout', (req, res) => {
-    res.redirect('/');
-})
-
-/********* Socket.io *********/
-
-/*** 
- * Handles communication of messages between a socket
- * within the ioWeb ('/user') namespace
- */
-ioWeb.on('connection', (socket) => {
-    console.log('a web user has connected');
-
-    socket.on('send command', (data) => {
-        console.log("Received command from user");
-        console.log(data);
-        ioWeb.emit('send command confirm', 'Command received from server');
-        ioPi.emit('send command', data);
+/********* Socket.io Stuff *********/
+//* web namespace */
+ioWeb.on('connection', function(socket){
+    console.log("a web user has connected.");
+    socket.on('send command', function(data){
+      console.log('command received');
+      ioPi.emit('send command', data);
     });
 });
-
-// Pi stuff
-ioPi.on('connection', (socket) => {
-    console.log('a pi has connected');
+/* pi namespace */
+ioPi.on('connection', function(socket){
+    console.log("a pi user has connected.");
+    socket.on('send command confirm', function(msg){
+      console.log("command confirmation received from Pi");
+      ioWeb.emit('send command confirm', msg);
+  });
 });
-
-server.listen(process.env.PORT || 8080, console.log('Ready at http://localhost:8080/'));
+let port = process.env.PORT || 8080;
+http.listen(port, function(){
+  console.log('listening on: ' + port);
+});
