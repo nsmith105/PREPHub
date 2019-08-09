3fe2d0f7a9813657bbe6927d9018119fa2fb2d3b
Robert Horrace <rhorrace95@gmail.com>
2019-08-08 20:16:10 -0700

    master-rhorrace

--- prephub-Server/public/js/local.js
+++ prephub-Server/public/js/local.js
@@ -18,12 +18,12 @@ $(document).ready(function () {
   let $container = $(".container");
 
   // Socket connection to web namespace '/user'
-  var socket = io("http://localhost:8080/web");
+  //var socket = io("http://localhost:8080/web");
 
   /**
    * This handles expanding/contracting the collapsibles
    */
-/*  $container.on("click", ".collapsible", function () {
+  $container.on("click", ".collapsible", function () {
     let $this = $(this);
     let content = $this.next();
     if (content.css("max-height") != "0px") {
@@ -33,43 +33,14 @@ $(document).ready(function () {
       $this.addClass("active");
       content.css("max-height", content.prop("scrollHeight"));
     }
-  });*/
-  /**
-   * Carousel stuff, subject to change to a different
-   * tech
-   
-  $("#twitter").slick({
-    dots: true,
-    infinite: false,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: false,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: false,
-          dots: true
-        }
-      }
-    ]
   });
+   
   /**** SOCKET.io related ****/
   /**
    * When a radio/light button is clicked, \
    * we grab the value and call send_data
    */
-  jQuery(".change-radio").on("click", function () {
+/*  jQuery(".change-radio").on("click", function () {
     let val = $(this).text();
     send_data("Change Radio", val);
   });
@@ -91,11 +62,7 @@ $(document).ready(function () {
   socket.on('connect', () => {
     console.log('This client successfully connected to the server');
   });
-
-  socket.on('twitter data', (data) => {
-    load_twitter(data);
-  });
-
+  */
   function openLang(evt, lang) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -110,14 +77,4 @@ $(document).ready(function () {
     evt.currentTarget.className += " active";
   }
 
-  /**
-   * When we pass the data in, it'll be a JSON string
-   * we need to parse it to get a Javascript obj
-   */
-  function load_twitter(data) {
-    $(".card").each(function(index, element) {
-      $(this).append($("<p class='text'></p>").text(data.tweets[index].text));
-      $(this).append($("<p class='date'></p>").text(data.tweets[index].date));
-    });
-  }
 });
