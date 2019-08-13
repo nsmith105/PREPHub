bfaa9abae9f167628efc9a7d0b7d4c7339f57fc0
Robert Horrace <rhorrace95@gmail.com>
2019-08-12 19:23:36 -0700

    Add files via upload

---
+++ prephub-hardware/prephub-Local/public/js/local.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  let $container = $(".container");
+
+  /**
+   * This handles expanding/contracting the collapsibles
+   */
+  $container.on("click", ".collapsible", function () {
+    let $this = $(this);
+    let content = $this.next();
+    if (content.css("max-height") != "0px") {
+      $this.removeClass("active");
+      content.css("max-height", 0);
+    } else {
+      $this.addClass("active");
+      content.css("max-height", content.prop("scrollHeight"));
+    }
+   
+  });
+});
